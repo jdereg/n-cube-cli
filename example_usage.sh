b6110eb4bbe6736143203f9dee9d3449e3ca71e7
Jim Kohl <jkohl2@gaig.com>
2016-12-13 14:19:10 -0500

    got rid of hard coded path in sh script

--- example_usage.sh
+++ example_usage.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-javaPath="c:/Program Files/Java/jdk1.8.0_66/jre/bin"
+#javaPath="c:/Program Files/Java/jdk1.8.0_66/jre/bin"
 
 USERNAME=$1
 PASSWORD=$2
 
-cookie=`"${javaPath}"/java -jar target/n-cube-cli-1.0.0-SNAPSHOT.jar nce-auth --u ${USERNAME} --p ${PASSWORD}`
-
+cookie=`java -jar target/n-cube-cli-1.0.0-SNAPSHOT.jar nce-auth --u ${USERNAME} --p ${PASSWORD}`
 echo "${cookie}"
-
-version=`"${javaPath}"/java -jar target/n-cube-1.0.0-SNAPSHOT.jar nce-getappversion --cookie ${cookie} --app Devops.Test`
-
+version=`java -jar target/n-cube-cli-1.0.0-SNAPSHOT.jar nce-getappversion --cookie ${cookie} --app Devops.Test`
 echo "${version}"
